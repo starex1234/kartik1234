6ef238322340925f0c4e43a04d8d1b2ffbf16309
kartik <kartikbhambu@gmail.com>
2018-12-29 15:23:36 +0530

    added by kartik

---
+++ Student_Portal/Controllers/MainController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Student_Portal.Models;
+namespace Student_Portal.Controllers
+{
+    public class MainController : Controller
+    {
+        // GET: Main
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index (EmployeeDetail emp)
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Student_Portal/Models/EmployeeDetail.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+namespace Student_Portal.Models
+{
+    public class EmployeeDetail
+    {
+        [Key]
+        public int EmpId { get; set; }
+
+        [Required(ErrorMessage ="Field  is required")]
+        public string EmpName { get; set; }
+
+        [Required(ErrorMessage = "Field is required")]
+        public string EmpEmail { get; set; }
+
+        [Required(ErrorMessage = "Field is required")]
+        public string EmpDep { get; set; }
+
+        [Required(ErrorMessage = "Field is required")]
+        public string EmpCompany { get; set; }
+
+       
+    }
+}
